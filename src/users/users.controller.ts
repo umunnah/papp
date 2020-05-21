cd90f69248d11b7d0baab3f792e0bb38fb5a3f24
Umunnah <ualawrence@yahoo.com>
2020-05-21 16:32:05 +0100

    User service with dummy data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import  { User} from './interface/user.interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    findAll() : User[] {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    findOneUser(@Param('id') id: string) : User {
+        return this.usersService.findOneUser(id);
+    }
+
+    @Post()
+    createUser(@Body() createbodyto: CreateUserDto) : string {
+        return `${createbodyto.email}`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: CreateUserDto): string {
+        return `user with ${id} has been update with ${updateUserDto}`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): string {
+        return `user with ${id} has been deleted`;
+    }
+}
