cbe9b5f7e92f582239d6ad32e158306a1b7a2089
Umunnah <ualawrence@yahoo.com>
2020-05-23 17:06:35 +0100

    new structure

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+    imports: []
+})
 export class UsersModule {}
